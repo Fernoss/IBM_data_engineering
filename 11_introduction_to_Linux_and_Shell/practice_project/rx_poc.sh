7f19d85e9f4a8926855e53b5c122dda1b19dc181
Joonas Fernando Koskinen <98735275+Fernoss@users.noreply.github.com>
2023-12-19 16:17:05 +0200

    Add files via upload

---
+++ 11_introduction_to_Linux_and_Shell/practice_project/rx_poc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Variables
+city=Casablanca
+
+
+# Get data
+curl -s wttr.in/$city?T --output weather_report
+
+#To extract Current Temperature
+obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
+echo "The current Temperature of $city: $obs_temp"
+
+# To extract the forecast tempearature for noon tomorrow
+fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
+echo "The forecasted temperature for noon tomorrow for $city : $fc_temp C"
+
+# store time
+hour=$(TZ='Morocco/Casablanca' date -u +%H) 
+day=$(TZ='Morocco/Casablanca' date -u +%d) 
+month=$(TZ='Morocco/Casablanca' date +%m)
+year=$(TZ='Morocco/Casablanca' date +%Y)
+
+# merge the data to the created table
+record=$(echo -e "$year\t$month\t$day\t$obs_temp\t$fc_temp C")
+echo $record>>rx_poc.log
