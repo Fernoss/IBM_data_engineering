cc3acf07fa61580c37ab2159d4381bba3525fe29
Joonas Fernando Koskinen <98735275+Fernoss@users.noreply.github.com>
2024-01-17 13:01:41 +0200

    Add files via upload

---
+++ 9_ETL_and_Data_Pipelines_with_Shell_Airflow_and_Kafka/Airflow_practice/hands-on/dags/ETL_Server_Access_Log_Processing.py
@@ -0,0 +1,63 @@
+# task 1
+from datetime import timedelta
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+# Operators; we need this to write tasks!
+from airflow.operators.bash_operator import BashOperator
+# This makes scheduling easy
+from airflow.utils.dates import days_ago
+
+# task 2
+#defining DAG arguments
+
+default_args = {
+    'owner': 'Ramesh Sannareddy',
+    'start_date': days_ago(0),
+    'email': ['ramesh@somemail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+# task 3
+# defining the DAG
+
+dag = DAG(
+    'ETL_Server_Access_Log_Processing',
+    default_args=default_args,
+    description='My first DAG',
+    schedule_interval=timedelta(days=1),
+)
+# define the tasks
+
+# task 4
+# define the task 'download'
+download = BashOperator(
+    task_id='download',
+    bash_command='wget "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Apache%20Airflow/Build%20a%20DAG%20using%20Airflow/web-server-access-log.txt"',
+    dag=dag,
+)
+# task 5
+# define the task 'extract'
+extract = BashOperator(
+    task_id='extract',
+    bash_command='cut -f1,4 -d"#" web-server-access-log.txt > /home/project/airflow/dags/extracted.txt',
+    dag=dag,
+)
+# task 6
+# define the task 'transform'
+transform = BashOperator(
+    task_id='transform',
+    bash_command='tr "[a-z]" "[A-Z]" < /home/project/airflow/dags/extracted.txt > /home/project/airflow/dags/capitalized.txt',
+    dag=dag,
+)
+# task 7
+# define the task 'load'
+load = BashOperator(
+    task_id='load',
+    bash_command='zip log.zip capitalized.txt' ,
+    dag=dag,
+)
+# task 8
+# task pipeline
+download >> extract >> transform >> load
